87f96884faac1e13c8df1d73a1d63b9ae166488a
linksxup <linksxup@gmail.com>
2021-06-16 00:52:25 +0100

    Update calculate result166.blade.php

--- calculate result166.blade.php
+++ calculate result166.blade.php
@@ -3,8 +3,7 @@
 <head>
 <title>Welcome</title>
 
-<link rel="stylesheet" type="text/css"
-href="sheet3.css">
+<link rel="stylesheet" type="text/css" href="sheet3.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- icons -->
     <link rel="shortcut icon" href="icon.png" />
