57e9a8f2a51df6dcab4c22a99f51c8319c9d0df5
linksxup <linksxup@gmail.com>
2021-06-16 01:32:49 +0100

    Update account.php

--- account.php
+++ account.php
@@ -5,8 +5,6 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="shortcut icon" href="icon.png" />
-
-
 <title>My account</title>		
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
